64c8107b41e29c37f03fef307eb3658a263527f3
JuanitoAlcachofa123 <juanchavezMGmaster@gmail.com>
2023-08-28 09:19:21 -0400

    VISTAS DEL PROYECTO

--- vista.js
+++ vista.js
@@ -2,18 +2,74 @@ const express = require("express");
 const conexion = require("./conexion");
 const router = express.Router();
 
-//VISTAS:
-router.get('/vista', (req, res) => {
+//REPORTE GENERAL DE LO MAS IMPORTANTE
+router.get('/vista/general', (req, res) => {
     let sql = 'SELECT * FROM VistaIntercambios';
     
     conexion.query(sql, (err, result) => {
         if (err) {
             console.log(err.message);
-            res.json({ mensaje: 'Error al obtener los datos de la vista VistaIntercambios' });
+            res.json({ mensaje: 'Error al obtener los datos de los reportes Generales' });
         } else {
             res.json(result);
         }
     });
 });
+//REPORTE DE HABILIDADES JUNTO A USUARIOS
+router.get('/vista/usuarios', (req, res) => {
+    let sql = 'select * from VistaUsuariosHabilidades;';
+    
+    conexion.query(sql, (err, result) => {
+        if (err) {
+            console.log(err.message);
+            res.json({ mensaje: 'Error al obtener los datos de los reportes Generales' });
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+//REPORTE DE USUARIOS ACTIVOS
+router.get('/vista/usuarios/activos', (req, res) => {
+    let sql = 'select * from VistaUsuariosActivos;';
+    
+    conexion.query(sql, (err, result) => {
+        if (err) {
+            console.log(err.message);
+            res.json({ mensaje: 'Error al obtener los datos de los reportes Generales' });
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+//REPORTE DE USUARIOS INACTIVOS
+router.get('/vista/usuarios/inactivos', (req, res) => {
+    let sql = 'select * from VistaUsuariosInactivos;';
+    
+    conexion.query(sql, (err, result) => {
+        if (err) {
+            console.log(err.message);
+            res.json({ mensaje: 'Error al obtener los datos de los reportes Generales' });
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+//REPORTE DE USUARIOS SIN INTERCAMBIOS
+router.get('/vista/usuarios/sin/intercambios', (req, res) => {
+    let sql = 'select * from UsuariosSinIntercambios;';
+    
+    conexion.query(sql, (err, result) => {
+        if (err) {
+            console.log(err.message);
+            res.json({ mensaje: 'Error al obtener los datos de los reportes Generales' });
+        } else {
+            res.json(result);
+        }
+    });
+});
+
 
 module.exports = router;
