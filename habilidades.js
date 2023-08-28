45ef60cb5d55d9f2b9512b1baf3fffebc706d5ce
JuanitoAlcachofa123 <juanchavezMGmaster@gmail.com>
2023-08-28 09:02:31 -0400

    HABILIDADES DE PROYECTO

--- habilidades.js
+++ habilidades.js
@@ -45,4 +45,45 @@ router.get('/habilidades', (req, res) => {
     });
 });
 
+// Endpoint para eliminar una habilidad por ID
+router.delete('/habilidades/:idHabilidad', (req, res) => {
+    const idHabilidad = req.params.idHabilidad;
+
+    let sql = 'DELETE FROM Habilidades WHERE IDHabilidad = ?';
+    
+    conexion.query(sql, [idHabilidad], (err, result) => {
+        if (err) {
+            console.log(err.message);
+            res.json({ mensaje: 'Error al eliminar la habilidad' });
+        } else {
+            if (result.affectedRows === 0) {
+                res.json({ mensaje: 'Habilidad no encontrada' });
+            } else {
+                res.json({ mensaje: 'Habilidad eliminada exitosamente' });
+            }
+        }
+    });
+});
+
+// Endpoint para actualizar una habilidad por ID
+router.put('/habilidades/:idHabilidad', (req, res) => {
+    const idHabilidad = req.params.idHabilidad;
+    const datosActualizados = req.body;
+
+    let sql = 'UPDATE Habilidades SET ? WHERE IDHabilidad = ?';
+    
+    conexion.query(sql, [datosActualizados, idHabilidad], (err, result) => {
+        if (err) {
+            console.log(err.message);
+            res.json({ mensaje: 'Error al actualizar la habilidad' });
+        } else {
+            if (result.affectedRows === 0) {
+                res.json({ mensaje: 'Habilidad no encontrada' });
+            } else {
+                res.json({ mensaje: 'Habilidad actualizada exitosamente' });
+            }
+        }
+    });
+});
+
 module.exports = router;
